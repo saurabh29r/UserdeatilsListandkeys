dbe465b7abbf5e9506c4876821c2a5029276a734
Saurabh <saurabh100rab002@gmail.com>
2023-09-11 19:15:23 +0000

    c

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ const userDetails = {
  { imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
   name: 'Saurabh',
   role: 'Software Development Engineer'},
-  {
-      imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-  name: 'Gurav',
-  role: 'Software Development Engineer'
-  }
+
 }
 
 const App = () => <h1>Users List App</h1>
