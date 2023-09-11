142bb152f587a711d3a04bbbfeddb622c3977178
Saurabh <saurabh100rab002@gmail.com>
2023-09-11 18:45:44 +0000

    intial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const userDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Saurabh',
+  role: 'Software Development Engineer',
+}
+
+const App = () => <h1>Users List App</h1>
+
+export default App
